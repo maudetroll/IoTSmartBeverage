23cad4d9e23259598390a2cc9b241d5807b338b7
mulc <buermarc2@gmail.com>
2021-05-18 11:26:25 +0200

    feat(update_server) handle update get request
    Get request on `/update` checks for correct headers and corresponding
    values. Only send update when version differs and secret matches.
    
    Add `./test-request.sh` to check if the server handles requests
    correctly. Usage described in `readme.txt`.

--- update_server/readme.txt
+++ update_server/readme.txt
@@ -8,6 +8,17 @@ Certificates
 ---
 
 Self signed cert for `localhost` is contained in simlinked directory
-`./self-signed-cert`.
+`./self-signed-cert`. Secret which authorizes the requesting client is now
+within a separate module not contained in the repo. Path to the binary which
+should be send to requesting ESPs is hardcoded 
+(at the moment: `./srv/esp-builds/latest.ino.bin.signed`)
+
+Send test request
+---
+Use `./test-request.sh` to test the running update server. Server should listen
+on `localhost:8080`, the self signed certification is needed and the base64
+encoded version and secret should be passed to the script via an environment
+variable.
+
 TODO:
 - private part isn't contained in the repo yet, so this code won't run
---
+++ update_server/test-request.sh
@@ -0,0 +1,27 @@
+# First should not work -> MD5 matches, 
+curl \
+-H "User-Agent: ESP8266-http-Update" \
+-H "x-ESP8266-STA-MAC: 18:FE:AA:AA:AA:AA" \
+-H "x-ESP8266-AP-MAC:  1A:FE:AA:AA:AA:AA" \
+-H "x-ESP8266-free-space: 671744" \
+-H "x-ESP8266-sketch-size: 373940" \
+-H "x-ESP8266-sketch-md5: ${MD5_MATCH}" \
+-H "x-ESP8266-chip-size: 4194304" \
+-H "x-ESP8266-sdk-version: 1.3.0" \
+-H "x-ESP8266-version: ${BASE64_VERSION_SECRET}" \
+-H "x-ESP8266-mode: sketch" \
+https://localhost:8080/update --cacert ./self-signed-cert/cert.pem > should-not-work
+
+# Second should work -> MD5 does not matches
+curl \
+-H "User-Agent: ESP8266-http-Update" \
+-H "x-ESP8266-STA-MAC: 18:FE:AA:AA:AA:AA" \
+-H "x-ESP8266-AP-MAC:  1A:FE:AA:AA:AA:AA" \
+-H "x-ESP8266-free-space: 671744" \
+-H "x-ESP8266-sketch-size: 373940" \
+-H "x-ESP8266-sketch-md5: aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" \
+-H "x-ESP8266-chip-size: 4194304" \
+-H "x-ESP8266-sdk-version: 1.3.0" \
+-H "x-ESP8266-version: ${BASE64_VERSION_SECRET}" \
+-H "x-ESP8266-mode: sketch" \
+https://localhost:8080/update --cacert ./self-signed-cert/cert.pem > should-work
